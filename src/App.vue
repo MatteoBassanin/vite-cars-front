6122fd1b3b4a54c75f42aeb1062367b6f9a911a9
leonardoserra <serra.leonardo92@gmail.com>
2023-06-05 17:26:01 +0200

    Aggiunto router-link

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script >
-
-
-
+import AppNav from './components/AppNav.vue';
 export default {
 
+  components: {
+    AppNav
+  }
 }
 </script>
 
 <template>
   <div class="container">
     <div class="row">
+      <AppNav></AppNav>
       <router-view></router-view>
 
     </div>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppNav'
+}
+</script>
+<template>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <router-link class="navbar-brand nav-link" :to="{ name: 'index' }">Cars List</router-link>
+        </div>
+    </nav>
+</template>
+<style></style>
+  
